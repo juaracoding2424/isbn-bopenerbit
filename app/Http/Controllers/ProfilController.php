b9ee928d2a2677bdf99ad209703670d39ac31c0a
juaracoding2424 <vd.tyaz@gmail.com>
2024-09-25 12:10:28 +0700

    fix join to left join

--- app/Http/Controllers/ProfilController.php
+++ app/Http/Controllers/ProfilController.php
@@ -15,15 +15,15 @@ class ProfilController extends Controller
                         P.NAME NAMA_PENERBIT, P.EMAIL1 ADMIN_EMAIL, P.EMAIL2 ALTERNATE_EMAIL, P.PROVINCE_ID, P.CITY_ID, P.DISTRICT_ID, P.VILLAGE_ID,
                         P.ALAMAT ALAMAT_PENERBIT, P.ID, P.TELP1 ADMIN_PHONE, P.TELP2 ALTERNATE_PHONE, P.NAMA_GEDUNG, P.ISBN_USER_NAME USER_NAME
               FROM PENERBIT P 
-                        JOIN PROPINSI on propinsi.id = P.PROVINCE_ID
-                        JOIN KABUPATEN ON KABUPATEN.id = P.CITY_ID
+                        LEFT JOIN PROPINSI on propinsi.id = P.PROVINCE_ID
+                        LEFT JOIN KABUPATEN ON KABUPATEN.id = P.CITY_ID
                         WHERE P.ID=$id";
             $data = kurl("get","getlistraw", "", $sql, 'sql', '')["Data"]["Items"][0];
         } else {
             $sql = "SELECT 'NOTVALID' STATUS, IR.*,
                         PROPINSI.NAMAPROPINSI, KABUPATEN.NAMAKAB FROM ISBN_REGISTRASI_PENERBIT IR 
-                        JOIN PROPINSI on propinsi.id = IR.PROVINCE_ID
-                        JOIN KABUPATEN ON KABUPATEN.id = IR.CITY_ID
+                        LEFT JOIN PROPINSI on propinsi.id = IR.PROVINCE_ID
+                        LEFT JOIN KABUPATEN ON KABUPATEN.id = IR.CITY_ID
                         WHERE IR.ID=$id";
             $data = kurl("get","getlistraw", "", $sql, 'sql', '')["Data"]["Items"][0];
         }
@@ -34,7 +34,6 @@ class ProfilController extends Controller
     {
         try {
             $id = session('penerbit')['ID'];
-            \Log::info(session('penerbit'));
             $validator = \Validator::make(request()->all(),[
                 'name' => 'required:min:8',
                 'username' => 'required|min:6',
@@ -73,12 +72,14 @@ class ProfilController extends Controller
                         [ "name"=>"TELP1", "Value"=> request('phone')],
                         [ "name"=>"ALAMAT", "Value"=> request('alamat_penerbit')],
                         [ "name"=>"NAMA_GEDUNG", "Value"=> request('nama_gedung') ],
-                        [ "name"=>"PROVINCE_ID", "Value"=> request('province') ],
+                        [ "name"=>"PROVINCE_ID", "Value"=> request('provinsi') ],
                         [ "name"=>"CITY_ID", "Value"=> request('kabkot') ],
                         [ "name"=>"DISTRICT_ID", "Value"=> request('kecamatan') ],
                         [ "name"=>"VILLAGE_ID", "Value"=> request('kelurahan') ],
+                        [ "name"=>"UPDATEBY", "Value"=> session('penerbit')["USERNAME"]], //nama user penerbit
+                        [ "name"=>"UPDATETERMINAL", "Value"=> \Request::ip()]
                     ];
-                    \Log::info(config('app.inlis_api_url') ."?token=" . config('app.inlis_api_token')."&op=update&table=PENERBIT&id=$id&issavehistory=1&ListUpdateItem=" . urlencode(json_encode($ListData)));
+                    //\Log::info(config('app.inlis_api_url') ."?token=" . config('app.inlis_api_token')."&op=update&table=PENERBIT&id=$id&issavehistory=1&ListUpdateItem=" . urlencode(json_encode($ListData)));
                     $res =  Http::post(config('app.inlis_api_url') ."?token=" . config('app.inlis_api_token')."&op=update&table=PENERBIT&id=$id&issavehistory=1&ListUpdateItem=" . urlencode(json_encode($ListData)));
                 } else {
                     $ListData = [
@@ -87,14 +88,16 @@ class ProfilController extends Controller
                         [ "name"=>"ADMIN_PHONE", "Value"=> request('phone')],
                         [ "name"=>"ALAMAT_PENERBIT", "Value"=> request('alamat_penerbit')],
                         [ "name"=>"NAMA_GEDUNG", "Value"=> request('nama_gedung') ],
-                        [ "name"=>"PROVINCE_ID", "Value"=> request('province') ],
+                        [ "name"=>"PROVINCE_ID", "Value"=> request('provinsi') ],
                         [ "name"=>"CITY_ID", "Value"=> request('kabkot') ],
                         [ "name"=>"DISTRICT_ID", "Value"=> request('kecamatan') ],
                         [ "name"=>"VILLAGE_ID", "Value"=> request('kelurahan') ],
+                        [ "name"=>"UPDATEBY", "Value"=> session('penerbit')["USERNAME"]], //nama user penerbit
+                        [ "name"=>"UPDATETERMINAL", "Value"=> \Request::ip()]
                     ];
                     $res =  Http::post(config('app.inlis_api_url') ."?token=" . config('app.inlis_api_token')."&op=update&table=ISBN_REGISTRASI_PENERBIT&id=$id&issavehistory=1&ListUpdateItem=" . urlencode(json_encode($ListData)));
                 }
-                \Log::info($res);
+                //\Log::info($res);
                 //INSERT HISTORY
                
                 //\Log::info(config('app.inlis_api_url') . "?token=" . config('app.inlis_api_token') . "&op=add&table=HISTORYDATA&ListAddItem=" . urlencode(json_encode($history)));
